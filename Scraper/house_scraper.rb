6be8b47056756aa4aeecc4fbfbdd262a534948b5
Andy M <andy.dude@gmail.com>
2015-03-28 12:28:14 -0500

    Updated house scraper

--- Scraper/house_scraper.rb
+++ Scraper/house_scraper.rb
@@ -18,6 +18,7 @@ class HouseScraper
 
     #this is the box in the top right
     infobox = page.at_css('.infobox')
+    return nil if infobox.nil? 
     image = infobox.at_css(".infobox-image img")
 
     house = {}
@@ -30,6 +31,8 @@ class HouseScraper
     house[:overlord] = 'none'
     house[:cadets] = []
 
+    puts house[:name]
+
     info_rows = infobox.css('tr')
     info_rows.each do |row|
  
@@ -37,8 +40,8 @@ class HouseScraper
       value = row.at_css('td')
 
       next if label.nil? || label.text.nil? || value.nil?
-
-      puts label.text
+      #puts label.text
+      
       case label.text.downcase
         when 'seat'
 
@@ -47,14 +50,20 @@ class HouseScraper
         when 'current lord'
           if (value.text.downcase.include? ('unknown') or value.text.downcase.include? ('extinct'))
             house[:lord] = 'none'
-          else
+          elsif !value.at_css('a').nil?
             house[:lord] = value.at_css('a')[:href]
+          else
+            house[:lord] = value.text
           end
 
         when 'region'
-          house[:region] = value.at_css('a')[:href]
+          if value.at_css('a').nil?
+            house[:region] = 'exiled'
+          else
+            house[:region] = value.at_css('a')[:href]
+          end
         when 'overlord'
-          if !value.text.include? 'none'
+          if !value.text.downcase.include? 'none' and !value.at_css('a').nil?
             house[:overlord] = value.at_css('a')[:href]
           end
 
@@ -69,17 +78,22 @@ class HouseScraper
     return house
   end
 
-  def retrieve_all_houses
+  def retrieve_all_houses (output_file_name)
      #.navbox-list a
      #find all the links and call retrieve_house on all of them
      page = Nokogiri::HTML(open(ALL_HOUSES))
 
      house_links = page.css(".navbox-list a")
+     houses = []
 
      house_links.each do |link|
       house = retrieve_house(link[:href])
+
+      houses << house if !house.nil?
      end
 
+     file = File.open(output_file_name, "w")
+     file.write(houses.to_json)
   end
 
 
@@ -88,15 +102,15 @@ end
 scraper = HouseScraper.new
 
 #if ARGV.count >= 1
-  puts scraper.retrieve_house('http://awoiaf.westeros.org/index.php/House_Reyne').to_json
-  puts scraper.retrieve_house('http://awoiaf.westeros.org/index.php/House_Stark').to_json
-  puts scraper.retrieve_house('http://awoiaf.westeros.org/index.php/House_Tully').to_json
-  puts scraper.retrieve_house('http://awoiaf.westeros.org/index.php/House_Baratheon_of_Dragonstone').to_json
-  puts scraper.retrieve_house('http://awoiaf.westeros.org/index.php/House_Baelish').to_json
-  puts scraper.retrieve_house('http://awoiaf.westeros.org/index.php/House_Baelish_of_Harrenhal').to_json
-  puts scraper.retrieve_house('http://awoiaf.westeros.org/index.php/House_Baratheon_of_King%27s_Landing').to_json
+  # puts scraper.retrieve_house('http://awoiaf.westeros.org/index.php/House_Reyne').to_json
+  # puts scraper.retrieve_house('http://awoiaf.westeros.org/index.php/House_Stark').to_json
+  # puts scraper.retrieve_house('http://awoiaf.westeros.org/index.php/House_Tully').to_json
+  # puts scraper.retrieve_house('http://awoiaf.westeros.org/index.php/House_Baratheon_of_Dragonstone').to_json
+  # puts scraper.retrieve_house('http://awoiaf.westeros.org/index.php/House_Baelish').to_json
+  # puts scraper.retrieve_house('http://awoiaf.westeros.org/index.php/House_Baelish_of_Harrenhal').to_json
+  # puts scraper.retrieve_house('http://awoiaf.westeros.org/index.php/House_Baratheon_of_King%27s_Landing').to_json
 
 #else
-
+scraper.retrieve_all_houses ('houses.json')
 #end
 
